03fdf5d3ba65cc8d9af14d2a0b0303660550d296
Michael Le <me@michael1e.com>
2016-01-29 17:36:33 -0800

    finish part 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // gulpfile.js
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+var gulp              = require('gulp');
+var browserSync       = require('browser-sync');
+var nunjucksRender 		= require('gulp-nunjucks-render'); // importing the plugin
 
 gulp.task('serve', function() {
     console.log('running server');
@@ -9,6 +10,19 @@ gulp.task('serve', function() {
             baseDir: 'app'
         }
     });
+});
+
+// writing up the gulp nunjucks task
+gulp.task('nunjucks', function() {
+  console.log('nunjucking');
+
+  // configuring the templates folder for nunjucks
+  nunjucksRender.nunjucks.configure(['app/templates/']);
+
+  // get the pages files
+  return gulp.src('app/pages/**/*.+(html)')
+    .pipe(nunjucksRender())
+    .pipe(gulp.dest('app'))
 });
 
 //default task to be run with gulp
